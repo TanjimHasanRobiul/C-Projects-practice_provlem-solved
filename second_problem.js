9d36b1357da51ab2dccc099b990bda8525a7f244
Tanjimhasanrobiul <dvillage.pavelab@gmail.com>
2022-09-20 18:52:32 +0600

    second pvlam solved

---
+++ second_problem.js
@@ -0,0 +1,14 @@
+var math = 75.25;
+var bio = 65;
+var chemistry = 80;
+var physics = 35.45;
+var English = 99.50;
+
+var totalMark = math + bio + chemistry + physics + English;
+var AverageMark = totalMark / 5;
+let total = AverageMark;
+let nowMarks = total.toFixed(2);
+console.log(nowMarks);
+
+
+
