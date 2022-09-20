48da17941e86a1f98a7f6d8f1162cb01eeefef3a
Tanjimhasanrobiul <dvillage.pavelab@gmail.com>
2022-09-20 18:20:24 +0600

    first problem solved

---
+++ math.js
@@ -0,0 +1,8 @@
+var totalAmount = 1000;
+var mangoPrice = 200;
+var gripsPrice = 300;
+
+var totalCost = mangoPrice + gripsPrice;
+var nowTotalAmount = totalAmount - totalCost;
+
+console.log(nowTotalAmount);
